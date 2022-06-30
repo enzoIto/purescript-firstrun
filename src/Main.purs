9fd9059ed370711c30d597588b4ef5a6579b33f3
Enzo Ito <enzo.ito@madeiramadeira.com.br>
2022-06-30 15:44:51 -0300

    solving a Euler Problem

--- src/Main.purs
+++ src/Main.purs
@@ -2,9 +2,8 @@ module Main where
 
 import Prelude
 
-import Effect (Effect)
 import Effect.Console (log)
+import Euler (answer)
 
-main :: Effect Unit
 main = do
-  log "🍝"
+  log("The answer is " <> show answer)
